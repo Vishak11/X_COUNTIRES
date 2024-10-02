fd747980fe209b0d73340d7c9b2f8a0db4911dfb
Vishak11 <vishakvn007@gmail.com>
2024-10-02 13:31:46 +0530

    new commit

---
+++ x_countires/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [image, setImage] = useState([]);
+  async function fetchCountryImages() {
+    const data = await fetch(
+      "https://xcountries-backend.azurewebsites.net/all"
+    );
+    const countryDetails = await data.json();
+    console.log(countryDetails);
+    setImage(countryDetails);
+  }
+  useEffect(() => {
+    fetchCountryImages();
+  }, []);
+  return (
+    <div className="flag-container">
+      {image.map((country, index) => (
+        <div key={index} className="flag-item">
+          <img src={country.flag} alt={`${country.name} flag`} />
+          <p>{country.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
